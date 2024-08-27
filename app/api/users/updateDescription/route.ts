2c5abd25ff16b745efc7e3bf30bf99bd06e05f01
Raul Marco Cano <rawraul@outlook.com>
2024-08-27 01:41:23 +0100

    validate empty description

--- app/api/users/updateDescription/route.ts
+++ app/api/users/updateDescription/route.ts
@@ -6,18 +6,23 @@ import User from "@/models/User";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/lib/auth";
 
-export async function PUT(request: any) {
+export async function PUT(request: Request) {
   try {
     const session = await getServerSession(authOptions);
 
     if (!session || !session.user) {
       return new Response(JSON.stringify({ error: "Not authenticated" }), { status: 401 });
     }
-   
 
     const userId = session.user._id;
     const { description } = await request.json();
 
+    // Validate description field
+    if (!description || description.trim() === "") {
+      return new Response(JSON.stringify({ error: "Description is required" }), { status: 400 });
+    }
+
+    // Connect to the database and find the user by ID to update the description
     await connectDB();
 
     const user = await User.findById(userId);
