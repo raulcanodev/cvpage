1cd1aba4d119cc41050aa477a78c7e9eadea3f7f
Raul Marco Cano <rawraul@outlook.com>
2024-10-07 11:40:26 +0100

    not found page

--- app/[customDomain]/not-found.tsx
+++ app/[customDomain]/not-found.tsx
@@ -1,25 +1,8 @@
-"use client"; 
-import React, { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { ArrowRight } from "lucide-react"
-import Link from 'next/link'
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import config from '@/config';
 
-
-
-interface Props {
-  params: {
-    id?: string
-  }
-}
-
-export default function NotFoundPage({ params }: Props) {
-  const [username, setUsername] = useState('')
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-  }
-
+export default function NotFoundPage() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white flex flex-col justify-center items-center px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8 text-center">
@@ -27,43 +10,19 @@ export default function NotFoundPage({ params }: Props) {
           <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl">
             This domain doesn&apos;t exist, yet! 😉
           </h1>
-          <p className="mt-2 text-xl text-gray-600">
-            Create your hitme.to page
-          </p>
         </div>
-        <form onSubmit={handleSubmit} className="mt-8 space-y-6">
-          <div className="rounded-md shadow-sm -space-y-px">
-            <div className="flex rounded-md shadow-sm">
-              <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
-                hitme.to/
-              </span>
-              <Input
-                type="text"
-                required
-                className="flex-1 bg-gray-50 border-gray-300 min-w-0 block w-full px-3 py-2 rounded-none rounded-r-md focus:ring-blue-500 focus:border-blue-500"
-                placeholder={''}
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-          </div>
 
-          <div>
-            <Button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              Create your page
-              <ArrowRight className="ml-2 h-4 w-4" />
-            </Button>
-          </div>
-        </form>
         <div className="mt-6">
-          <Link href="/" className="font-medium text-blue-600 hover:text-blue-500">
-            Go back to homepage
-          </Link>
+          <Button
+            type="submit"
+            className="w-full text-white bg-zinc-950 hover:bg-zinc-700"
+          >
+            <Link href="/auth/register" className="font-medium">
+              Create your page
+            </Link>
+          </Button>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,17 +1,10 @@
-"use client"; 
-import React, { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { ArrowRight } from "lucide-react"
-import Link from 'next/link'
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
+import config from '@/config';
 
 export default function NotFoundPage() {
-  const [username, setUsername] = useState('')
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-  }
-
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white flex flex-col justify-center items-center px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8 text-center">
@@ -19,43 +12,20 @@ export default function NotFoundPage() {
           <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl">
             This domain doesn`&ldquo;`t exist, yet! 😉
           </h1>
-          <p className="mt-2 text-xl text-gray-600">
-            Create your hitme.to page
-          </p>
+          <p className="mt-2 text-xl text-gray-600">Create your {config.appName} page</p>
         </div>
-        <form onSubmit={handleSubmit} className="mt-8 space-y-6">
-          <div className="rounded-md shadow-sm -space-y-px">
-            <div className="flex rounded-md shadow-sm">
-              <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
-                hitme.to/
-              </span>
-              <Input
-                type="text"
-                required
-                className="flex-1 min-w-0 block w-full px-3 py-2 rounded-none rounded-r-md focus:ring-blue-500 focus:border-blue-500"
-                placeholder="your-username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-          </div>
 
-          <div>
-            <Button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              Create your page
-              <ArrowRight className="ml-2 h-4 w-4" />
-            </Button>
-          </div>
-        </form>
         <div className="mt-6">
-          <Link href="/" className="font-medium text-blue-600 hover:text-blue-500">
-            Go back to homepage
-          </Link>
+          <Button
+            type="submit"
+            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md"
+          >
+            <Link href="/auth/register" className="font-medium">
+              Create your page
+            </Link>
+          </Button>
         </div>
       </div>
     </div>
-  )
+  );
 }
