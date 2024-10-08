4dc880db270d9ca6b9564955e99ccd4fd4b35269
Raul Marco Cano <rawraul@outlook.com>
2024-10-08 22:18:09 +0100

    loader

--- app/dashboard/style/page.tsx
+++ app/dashboard/style/page.tsx
@@ -15,7 +15,7 @@ export default function DashboardEditProfile() {
   if (isLoading) {
     return (
       <>
-      <div className="flex items-center justify-center h-screen">
+      <div className="fixed inset-0 flex items-center justify-center">
         <LineWave
           visible={true}
           height="100"
