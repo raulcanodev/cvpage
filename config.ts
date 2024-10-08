f41e2dbe8a94b85e9191d4c18dd98c26349fc8ec
Raul Marco Cano <rawraul@outlook.com>
2024-10-08 10:37:20 +0100

    update domain

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ const config = {
   appName: 'Cvpage',
   appDescription: 'Create your own personal page with Cvpage and share it with the world',
   domainName: 'cvpage.to',
-  domainUrl: 'https://cvpage.to',
+  domainUrl: process.env.NODE_ENV === 'development' ? 'http://localhost' : 'https://cvpage.to',
 }
 
 export default config;
