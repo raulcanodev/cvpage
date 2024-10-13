fb3027df2df89ad80ec588639d22077868af3d48
Raul Marco Cano <rawraul@outlook.com>
2024-10-13 20:34:27 +0100

    black 404

--- app/[customDomain]/not-found.tsx
+++ app/[customDomain]/not-found.tsx
@@ -7,14 +7,14 @@ export default function NotFoundPage() {
     <div className="min-h-screen flex flex-col justify-center items-center px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8 text-center">
         <div>
-          <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl">
+          <h1 className="text-4xl font-extrabold sm:text-5xl">
             This domain doesn&apos;t exist, yet! 😉
           </h1>
         </div>
 
         <div className="mt-6">
           <Link href="/auth/register" className="font-medium">
-            <Button type="submit" className="w-full text-white bg-zinc-950 hover:bg-zinc-700">
+            <Button type="submit" className="w-full">
               Create your {config.appName}
             </Button>
           </Link>
