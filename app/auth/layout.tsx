6073989c13450408e592b2b4e40da7d1eaf22008
Raul Marco Cano <rawraul@outlook.com>
2024-10-16 17:34:27 +0100

    login

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,17 +1,14 @@
-import { SideBanner } from "./components/SideBanner";
+import { SideBanner } from './components/SideBanner';
 import { Toaster } from 'sonner';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-
   return (
-      <div className="min-h-screen text-white flex">
-        <Toaster position="top-center" />
-        {/* <SideBanner /> */}
-        <div className="flex-1 flex flex-col justify-center px-4 py-12 sm:px-6 lg:px-20 xl:px-24">
-        <div className="mx-auto w-full max-w-sm">
-        {children}
-        </div>
-        </div>
+    <div className="min-h-screen text-white flex">
+      <Toaster position="top-center" />
+      {/* <SideBanner /> */}
+      <div className="flex-1 flex flex-col justify-center px-4 py-12 sm:px-6 lg:px-20 xl:px-24">
+        <div className="mx-auto w-full max-w-sm">{children}</div>
       </div>
+    </div>
   );
 }
