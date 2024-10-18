b858bc8b204b3189488500f799efe917b892b77e
Raul Marco Cano <rawraul@outlook.com>
2024-10-18 10:18:11 +0100

    test logo login

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,5 +1,8 @@
 import { SideBanner } from './components/SideBanner';
 import { Toaster } from 'sonner';
+import Image from 'next/image';
+import cvpageWhite from '@/public/icon.png';
+
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,7 +10,13 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
       <Toaster position="top-center" />
       {/* <SideBanner /> */}
       <div className="flex-1 flex flex-col justify-center px-4 py-12 sm:px-6 lg:px-20 xl:px-24">
-        <div className="mx-auto w-full max-w-sm">{children}</div>
+        
+      
+        <div className="mx-auto w-full max-w-sm">
+        {/* <div className="flex items-center justify-center">
+          <Image src={cvpageWhite} alt="Logo" width={50} height={50} />
+        </div> */}
+          {children}</div>
       </div>
     </div>
   );
