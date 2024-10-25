0bbf2efabab59dcaedc17ccdcfa687821bfbd9f5
Raul Marco Cano <rawraul@outlook.com>
2024-10-26 00:37:20 +0100

    auth

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,6 +2,7 @@ import { Toaster } from 'sonner';
 import Image from 'next/image';
 import cvpageWhite from '@/public/icon.png';
 import config from '@/config';
+import Link from 'next/link';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -9,7 +10,9 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
       <Toaster position="top-center" />
       <div className="max-w-md w-full space-y-8 bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md">
         <div className="flex flex-col items-center">
-          <Image src={cvpageWhite} alt={config.appName} width={50} height={50} />
+          <Link className="pointer" href="/">
+            <Image src={cvpageWhite} alt={config.appName} width={50} height={50} />
+          </Link>
           {/* <h2 className="mt-6 text-3xl font-extrabold text-gray-900 dark:text-white">
             {config.appName}
           </h2> */}
