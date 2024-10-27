d0db291d42fa00606617770793479dde906d5df4
Raul Marco Cano <rawraul@outlook.com>
2024-10-27 22:47:43 +0000

    avoid banner if premium

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { useState, useEffect } from 'react';
 import { CreativeNavbar } from './components/layout';
 import { UserProvider, useUserContext } from './context/UserContext';
 import { Toaster } from 'sonner';
@@ -8,7 +8,14 @@ import { MinimalFooter } from '@/app/(marketing)/_components';
 
 function DashboardContent({ children }: { children: React.ReactNode }) {
   const { userData } = useUserContext();
-  const { premium, email, _id } = userData;
+  const [isUserPremium, setIsUserPremium] = useState(true); // Assume premium until proven otherwise
+
+  useEffect(() => {
+    // Check if userData exists and has a 'premium' status
+    if (userData && userData.premium !== undefined) {
+      setIsUserPremium(userData.premium); // Update only when userData.premium is defined
+    }
+  }, [userData]);
 
   return (
     <>
@@ -17,13 +24,16 @@ function DashboardContent({ children }: { children: React.ReactNode }) {
       <div className="min-h-screen pt-16 bg-gray-100 dark:bg-zinc-950">
         <div className="fixed left-0 right-0 top-0 z-50 flex justify-center">
           <div className="w-full bg-gray-100 dark:bg-zinc-950 px-4 py-2">
-            {!premium && <PremiumBanner email={email} userId={_id} />}
+            {/* Render banner only if the user is not premium */}
+            {!isUserPremium && (
+              <PremiumBanner email={userData.email} userId={userData._id} />
+            )}
             <div className="flex flex-col justify-center items-center h-14 pt-10">
               <CreativeNavbar />
             </div>
           </div>
         </div>
-        <div className={`container mx-auto max-w-[75rem] px-2 py-8 ${!premium && 'mt-8'}`}>
+        <div className={`container mx-auto max-w-[75rem] px-2 py-8 ${!isUserPremium && 'mt-8'}`}>
           {children}
         </div>
       </div>
@@ -31,7 +41,8 @@ function DashboardContent({ children }: { children: React.ReactNode }) {
         <MinimalFooter />
       </div>
 
-      {!premium && <PremiumPopup email={email} userId={_id} />}
+      {/* Render popup only if the user is not premium */}
+      {!isUserPremium && <PremiumPopup email={userData.email} userId={userData._id} />}
     </>
   );
 }
