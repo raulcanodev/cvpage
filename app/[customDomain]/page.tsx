2da163f6593d472592ee9fb7aad76d292667e45d
Raul Marco Cano <rawraul@outlook.com>
2024-10-28 21:21:07 +0000

    color

--- app/[customDomain]/page.tsx
+++ app/[customDomain]/page.tsx
@@ -42,12 +42,10 @@ export default async function UserProfilePage({ params }: Props) {
 
   const getBackgroundStyle = (color: string) => {
     switch (color) {
-      case 'midnight':
-        return 'absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]';
+      case 'monochrome':
+        return 'bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]';
       case 'plain dark':
         return 'bg-zinc-950 text-white';
-      default:
-        return 'bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]';
     }
   };
 
