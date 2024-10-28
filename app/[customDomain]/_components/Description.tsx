e21a781942a938613ffae9e79e19491c296038f3
Raul Marco Cano <rawraul@outlook.com>
2024-10-28 22:16:50 +0000

    pading

--- app/[customDomain]/_components/Description.tsx
+++ app/[customDomain]/_components/Description.tsx
@@ -6,6 +6,6 @@ interface DescriptionProps {
 export const Description = ({ description, pageColor }: DescriptionProps) => {
   const textColor = pageColor === 'monochrome' ? 'text-zinc-600' : 'text-gray-200';
   return (
-    <p className={`text-lg ${textColor} mb-2 px-2`}>{description}</p>
+    <p className={`text-lg ${textColor} mb-2 pl-2`}>{description}</p>
   )
 }
